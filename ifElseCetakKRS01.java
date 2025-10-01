1fc0cae5e6e69a4d587b060640365684a4373be2
adzrielllag-rgb <adzrielllag@gmail.com>
2025-10-01 19:47:34 +0700

    Modifikasi Percobaan 2

--- ifElseCetakKRS01.java
+++ ifElseCetakKRS01.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class ifElseCetakKRS01 {
-  public void main(String[] args) {
+  public static void main(String[] args) {
      Scanner sc = new Scanner(System.in);
      System.out.println("---Cetak KRS SIAKAD---");
      System.out.println("Masukan Semester ini :");
