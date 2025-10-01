c38727a3bee0602457800e1afdd4d17da1021cd3
adzrielllag-rgb <adzrielllag@gmail.com>
2025-10-01 18:51:30 +0700

    Modifikasi Percobaan 2

---
+++ Modifikasipercobaan1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Modifikasipercobaan1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("---Cetak KRS SIAKAD---");
+        System.out.print("Apakah UKT sudah lunas? (true/false): ");
+        boolean uktLunas = input.nextBoolean();
+
+        if(uktLunas) {
+          System.out.println("Pembayaran UKT Terverifikasi");
+          System.out.print("Silakan Cetak KRS dan Tanda Tangan DPA");
+          } else {
+          System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
+
+          input.close();
+          }
+    } 
+}
+  
